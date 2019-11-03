82da3b600b1685c248bbd37adf9e570a6fbf2488
Abhishek Samariya <asamariya@gmail.com>
2019-11-03 15:06:33 +1100

    added navCongrol to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,22 @@
 import React, {Component} from 'react';
+import mapbox from 'mapbox-gl';
 
 export default class Map extends Component {
+	componentDidMount() {
+		mapbox.accessToken =
+			'pk.eyJ1IjoiYXNhbWFyaXlhIiwiYSI6ImNqejNoMjVleDAzNDMzYnN2azM3anA5cnYifQ.Z3gBIGRLYkSZo3ZQVWQNMg';
+
+		const map = new mapbox.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [144.988, -37.803],
+			zoom: 15
+		});
+
+		const navControl = new mapbox.NavigationControl();
+		map.addControl(navControl, 'top-right');
+	}
 	render() {
-		return <div>map!!</div>;
+		return <div id="map"></div>;
 	}
 }
